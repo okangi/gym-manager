40df7d2fbf6a739a5396e43b8bbe2ccb4ad46ff1
Cyprian Okangi <makoricyprian@gmail.com>
2026-04-28 10:33:01 +0300

    Add explicit frontend URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const { validateIdParam } = require('./utils/validateId');
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'https://*.onrender.com'],
+  origin: ['http://localhost:5173', 'http://localhost:5174',  'https://gym-manager-frontend-agqh.onrender.com','http://localhost:3000', 'https://*.onrender.com'],
   credentials: true
 }));
 
